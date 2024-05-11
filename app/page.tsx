8f00339efc88a5f4dee8aa7e3d7e5dc33440a092
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-05-10 22:00:08 -0400

    styleTest

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
+export const metadata = {
+  title: 'Home',
+  description: 'Read my thoughts on software development, design, and more.',
+};
+
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-2 tracking-tighter">Joe Ward</h1>
-      <p className="mr-3 inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100">
-        PMP
-      </p>
-      <p className="mr-3 inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100">
-        Six-Sigma Green Belt
-      </p>
-      
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">Joe Ward</h1>
 
       <p className="prose prose-neutral dark:prose-invert">
         Joe Ward Joe Ward Joe Ward Joe Ward
       </p>
+      <p className="prose prose-neutral dark:prose-invert">
+        I'm an Operations specialist with extensive experience in analytics and business intelligence/growth strategy.
+      </p>
+
+      <ul className="mt-80">
+        <p className="mr-3 inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100">
+          PMP
+        </p>
+        <p className="mr-3 inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100">
+          Six-Sigma Green Belt
+        </p>
+      </ul>
     </section>
   );
 }
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,9 +1,15 @@
+export const metadata = {
+  title: 'Work',
+  description: 'Read my thoughts on software development, design, and more.',
+};
+
 export default function Page() {
+  
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is where your work experience goes.
+        Dallas BBQ.
       </p>
     </section>
   );
