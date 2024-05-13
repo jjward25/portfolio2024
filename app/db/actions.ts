7a04853e3e8d4ca7066a8eff4462105430b76e29
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-05-13 12:26:33 -0400

    dbCleanUp2

--- app/db/actions.ts
+++ app/db/actions.ts
@@ -1,8 +1,7 @@
 'use server';
 
-import { type Session } from 'next-auth';
 import { sql } from './postgres';
-import { revalidatePath, unstable_noStore as noStore } from 'next/cache';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function increment(slug: string) {
   noStore();
@@ -12,34 +11,4 @@ export async function increment(slug: string) {
     ON CONFLICT (slug)
     DO UPDATE SET count = views.count + 1
   `;
-}
-
-async function getSession(): Promise<Session> {
-  let session = await auth();
-  if (!session || !session.user) {
-    throw new Error('Unauthorized');
-  }
-
-  return session;
-}
-
-
-export async function deleteGuestbookEntries(selectedEntries: string[]) {
-  let session = await getSession();
-  let email = session.user?.email as string;
-
-  if (email !== 'me@leerob.io') {
-    throw new Error('Unauthorized');
-  }
-
-  let selectedEntriesAsNumbers = selectedEntries.map(Number);
-  let arrayLiteral = `{${selectedEntriesAsNumbers.join(',')}}`;
-
-  await sql`
-    DELETE FROM guestbook
-    WHERE id = ANY(${arrayLiteral}::int[])
-  `;
-
-  revalidatePath('/admin');
-  revalidatePath('/guestbook');
 }
