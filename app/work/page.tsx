3e9e946c7a4a64144cebe713b0076b7a0f890cce
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-05-15 11:31:10 -0400

    homeStyle2

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -11,10 +11,10 @@ export default function Page() {
       <div className='mt-3 mb-6 h-auto w-full border-2 border-neutral-700'></div>
 
       <ul className="mt-4 mb-4">
-        <p className="mr-3 inline-flex items-center rounded border  p-1 text-sm leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline">
+        <p className="mr-3 inline-flex items-center rounded border  p-1 text-sm leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-500 to-black">
           PMP
         </p>
-        <p className="mr-3 inline-flex items-center rounded border  p-1 text-sm leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline">
+        <p className="mr-3 inline-flex items-center rounded border  p-1 text-sm leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-500 to-black">
           Six-Sigma Green Belt
         </p>
       </ul>
