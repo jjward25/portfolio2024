06e685e2fe95e88930ec224c5f1943811342b023
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-05-16 10:55:12 -0400

    homeStyle5DRAFT

--- app/components/accordion.tsx
+++ app/components/accordion.tsx
@@ -10,7 +10,7 @@ function Accordion({ title, children }) {
 
   return (
     <div className="border border-white hover:border-neutral-400 rounded-lg mb-2 w-full bg-grad">
-      <div className={`bg-black hover:bg-gradient-to-r from-black to-neutral-700 border-b-white border-b-2 hover:border-neutral-400 cursor-pointer flex items-center justify-between rounded-lg  p-2 text-sm leading-4 h-9 ${isOpen ? 'border-b-amber-200 bg-gradient-to-r from-black to-neutral-500' : ''}`} onClick={toggleAccordion}>
+      <div className={`bg-black hover:bg-gradient-to-r from-black to-neutral-700 border-b-white border-b-2 hover:border-neutral-400 cursor-pointer flex items-center justify-between rounded-lg  p-2 text-sm leading-4 h-9 ${isOpen ? 'border-b-4 border-b-amber-200 bg-gradient-to-r from-black to-neutral-500' : ''}`} onClick={toggleAccordion}>
         <h3 className="text-md font-semibold no-underline">{title}</h3>
         <svg className={`w-5 h-5 text-white transition-transform duration-6000 transform ${isOpen ? 'rotate-180' : ''}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       <h1 className="font-medium text-2xl tracking-tighter">Hey 👋 I'm Joe Ward</h1>
       <div className='mt-3 mb-3 h-0.5 w-full  bg-gradient-to-r from-amber-200 to-black'> </div>
       
-      <div className='flex mt-8 mb-8 md:flex-wrap'>
+      <div className='flex mt-8 mb-8 flex-wrap place-content-center md:flex-nowrap'>
         <div className="flex-none w-32 h-32 ml-10 mr-10  rounded-full overflow-hidden">
           <img
             src='/prof.jpg'
@@ -21,33 +21,37 @@ export default function Page() {
           />
         </div>
 
-        <ul className="mt-4 mb-4 w-auto flex-auto  justify-center content-center">
-          <p className="mr-3 mb-3 inline-flex items-center rounded border  p-1 text-xs leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-700 to-black">
-            {`Operational Efficiency`}
-          </p>
-          <p className="mr-3 mb-3 inline-flex items-center rounded border  p-1 text-xs leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-700 to-black">
-            Process Improvement
-          </p>
-          <p className="mr-3 mb-3 inline-flex items-center rounded border  p-1 text-xs leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-700 to-black">
-            Growth / Sales / CS Analytics
-          </p>
-          <ul>
-            <p className="mr-3 mb-3 inline-flex items-center rounded border  p-1 text-xs leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-700 to-black">
-              People Manager
+        <div className='mt-9 md:m-auto w-full'>
+          <ul className="flex flex-nowrap w-full space-x-2">
+            <p className="w-full h-10 mb-3 inline-flex justify-center items-center rounded border p-1 text-xs leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-600 via-black to-neutral-600">
+              {`Operational Efficiency`}
             </p>
-            <p className="mr-3 mb-3 inline-flex items-center rounded border  p-1 text-xs leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-700 to-black">
-              Executive Partner
+            <p className="w-full h-10 mb-3 inline-flex justify-center items-center rounded border p-1 text-xs leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-600 via-black to-neutral-600">
+              Process Improvement
+            </p>
+            <p className="w-full h-10 mb-3 inline-flex justify-center items-center rounded border p-1 text-xs leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-600 via-black to-neutral-600">
+              Bus. Intelligence & Growth
             </p>
           </ul>
-        </ul>
-
+          <ul className='w-full'>
+              <p className="w-full justify-center mb-3 inline-flex items-center rounded border  p-1 text-xs leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-600 to-black">
+                People Manager
+              </p>
+              <p className="w-full justify-center  mb-3 inline-flex items-center rounded border  p-1 text-xs leading-4 border-neutral-700 bg-neutral-800 text-neutral-100 no-underline hover:bg-gradient-to-r from-neutral-600 to-black">
+                Executive Partner
+              </p>
+          </ul>
+        </div>
       </div>
       
-      <p className="prose prose-invert mb-10">
-        I'm an Operations specialist experienced in process improvement, change management, analytics and business intelligence/growth strategy. I'm creative, hands-on, collaborative, and a results-oriented project manager.
-      </p>
+      <div className='flex flex-nowrap h-auto'>
+        <div className=' md:mr-3 md:w-1 md:h-vh bg-gradient-to-r from-amber-200 to-black'> </div>
+        <p className="prose prose-invert">
+          I'm an Operations specialist experienced in process improvement, change management, analytics and business intelligence/growth strategy. I'm creative, hands-on, collaborative, and a results-oriented project manager.
+        </p>
+      </div>
 
-      <div className="w-full mx-auto my-5">
+      <div className="w-full mx-auto mt-10">
         <DynamicAccordion/>
       </div>
     </section>
