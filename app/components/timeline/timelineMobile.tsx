1ceaeb9f8f059af25a259a53dd0d0c240338535d
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-08-27 22:02:23 -0400

    Draft.1.5.7 - carouselV5Relative

--- app/components/timeline/timelineMobile.tsx
+++ app/components/timeline/timelineMobile.tsx
@@ -3,11 +3,11 @@ import { useState } from 'react';
 
 export default function Timeline() {
   return (
-    <div className="w-full max-w-full mx-auto mt-8 mb-8 bg-slate-100 rounded-xl p-4 text-black z-0">
+    <div className="w-full max-w-full mx-auto mt-8 mb-8 bg-slate-100 rounded-xl p-4 text-black z-0 ">
 
       <p className="font-semibold">Timeline</p>
 
-      <div className="flex flex-row max-w-full pb-3 overflow-auto no-scrollba group-hover:h-[400px]">
+      <div className="flex flex-row max-w-full pb-3 overflow-auto relative no-scroll group-hover:h-[400px]">
         
         {/* Year Column */}
         <div className="flex flex-col z-0 hover:z-20 group">
