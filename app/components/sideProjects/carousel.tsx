05fbaeb5a20340d59a2cfd09c049d97c933d34d4
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-08-28 11:55:48 -0400

    Draft.1.6.2 - YTTest

--- app/components/sideProjects/carousel.tsx
+++ app/components/sideProjects/carousel.tsx
@@ -17,13 +17,13 @@ export default function ProjectCarousel() {
       <div className="flex flex-row gap-4">
 
         {/** Category */}
-        <div className="flex flex-col bg-slate-100 p-2 rounded-xl">
+        <div className="flex flex-col bg-slate-100 p-2 rounded-xl max-w-[300px]">
             <p className="font-semibold text-md mb-2">Coding</p>
             {/** Project */}
-            <div className="text-sm border border-neutral-300 rounded-md p-3 mb-2 hover:scale-105 shadow-xl"> Project Name
-                <li className="mt-1">Description</li>
-                <li className="">Related Skills</li>
-                <div className="">img</div>
+            <div className="text-sm border border-neutral-300 rounded-md p-3 mb-2 hover:scale-105 shadow-xl"> Personal Webapp
+                <p className="mt-1"><em>A basic NextJS app using MongoDB and D3 charts to track tasks and daily routines.</em></p>
+                <p className="my-2 bg-cyan-950 rounded-xl text-white px-2">NextJS, MongoDB, D3, APIs</p>
+                <iframe width="260" height="145" src="https://www.youtube-nocookie.com/embed/guVl-h9NWZE?si=-uZrTaxUqFIzUCB9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         </div>
 
