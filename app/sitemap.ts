046cb5c4d72f8b7bc60079a5a2e6d776af2abaf0
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-10-01 14:14:18 -0400

    new

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 export default async function sitemap() {
 
   let routes = ['', '/blog', '/guestbook', '/uses', '/work'].map((route) => ({
-    url: `https://joeward.io${route}`,
+    url: `https://joeward.me${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
 
