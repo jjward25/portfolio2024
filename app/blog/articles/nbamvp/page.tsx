9ef06613089199ff1830076a1ffe5915ff4bd9f1
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-11-05 13:09:40 -0500

    mvpArticleEdits

--- app/blog/articles/nbamvp/page.tsx
+++ app/blog/articles/nbamvp/page.tsx
@@ -57,9 +57,10 @@ export default function TestBlogPost() {
             className="w-full h-auto object-cover cursor-pointer rounded-md border border-black"
             title="titlepic"
             />
-        <p className="">{`Above we see the Vegas odds for NBA MVP as of November 5th, 2024.  We're going to see if we can make a low-effort model that approximates this list, with minor exceptions.`}</p>
-        <p className="">{`These are betting odds so they take into account humans' irrational behaviours, like voting Anthony Edwards for MVP.  Statistically, based on age and overall performance I think he's an outlier on this list.`}</p>
-        <p className="mb-8">{`Similarly, Jokic has won 3 of the last 4 and his team is likely to severely underperform this year.  If Denver's Win Percentage still top 4 in the conference then the model should rate the Joker highly, but I don't expect Ant or Jokic to top our charts.`}</p>
+        <div className='mt-4'><a href="https://github.com/jjward25/nbaMvpPredictor">Github Repo</a></div>
+        <p className="">{`In the image above we have the Vegas odds for NBA MVP as of November 5th, 2024.  We're going to see if we can make a low-effort model that approximates this list, with minor exceptions.`}</p>
+        <p className="">{`Note: These are betting odds so they take into account peoples' irrational behaviours, like voting Anthony Edwards for MVP.  Statistically, based on age and overall performance I think he's an outlier on this list.`}</p>
+        <p className="mb-8">{`Similarly, Jokic has won 3 of the last 4 and his team is likely to severely underperform this year.  If Denver's Win Percentage is still top 4 in the conference then the model should rate the Joker highly, but I don't expect Ant or Jokic to top our charts.`}</p>
 
         {/** Data */}
         <DynamicAccordion title={`1. Gathering Data`} isOpenByDefault={false}>
@@ -71,9 +72,9 @@ export default function TestBlogPost() {
                 title="titlepic"
           /></div>
           <p className="">{`I use the NBA Stats API `}<a href="https://github.com/swar/nba_api/tree/master">found here</a>{` to pull all player stats.`}</p>
-          <p className="">{`Specifically, I pulled the top 50 players by efficiency and their season stats from the `}<a href="https://github.com/swar/nba_api/blob/master/src/nba_api/stats/endpoints/leagueleaders.py">leagueleaders</a>{` endpoint for the last 10 years. With that data I was able to create rank and per-game features.`}</p>
+          <p className="">{`Specifically, I pulled the top 50 players by efficiency and their season stats from the `}<a href="https://github.com/swar/nba_api/blob/master/src/nba_api/stats/endpoints/leagueleaders.py">leagueleaders</a>{` endpoint for the last 10 years. With that data I was able to create rank and per-game features as well.`}</p>
           <p className="">{`I then used the `}<a href="https://github.com/swar/nba_api/blob/master/src/nba_api/stats/endpoints/playerawards.py">playerawards</a>{` endpoint to tag MVPs and `}<a href="https://github.com/swar/nba_api/blob/master/src/nba_api/stats/endpoints/teamyearbyyearstats.py">teamyearbyyearstats</a> for Team metrics like Win Percentage and Conference Rank.</p>
-          <p className="">{`Finally, 3rd part libraries `}<a href="https://pypi.org/project/pytrends/">pytrend</a> and <a href="https://pypi.org/project/feedparser/">feedparser</a>{`helped us search for Google trend scores and ESPN RSS mentions for each player each season - unfortunately the RSS feed only goes back 14 days so we scrapped that.`}</p>
+          <p className="">{`Finally, 3rd party libraries `}<a href="https://pypi.org/project/pytrends/">pytrend</a> and <a href="https://pypi.org/project/feedparser/">feedparser</a>{` helped us search for Google trend scores and ESPN RSS mentions for each player each season - unfortunately the RSS feed only goes back 14 days so we scrapped that.`}</p>
         </DynamicAccordion>
 
 
@@ -97,13 +98,13 @@ export default function TestBlogPost() {
             />
           </div>
           <p className="">{`Correlation Matrixes can be helpful in spotting highly correlated and thus redundant features, while Scree plots give you their own estimation of the most important features.`}</p>
-          <p className="">{`In this case the Scree plot recommended ['REB', 'PTS', 'EFF', 'MIN_per_game', 'EFF_per_game', 'PTS_per_game_rank', 'EFF_per_game_rank', 'PTS_rank', 'EFF_rank', 'WIN_PCT'], which I trimmed down to ['MIN_per_game','EFF_per_game', 'PTS_per_game_rank','PTS_rank', 'EFF_rank','WIN_PCT'] for the final dataset.`}</p>
+          <p className="">{`The data recommended we use ['REB', 'PTS', 'EFF', 'MIN_per_game', 'EFF_per_game', 'PTS_per_game_rank', 'EFF_per_game_rank', 'PTS_rank', 'EFF_rank', 'WIN_PCT'], which I trimmed down to ['MIN_per_game','EFF_per_game', 'PTS_per_game_rank','PTS_rank', 'EFF_rank','WIN_PCT'] for the final dataset.`}</p>
         </DynamicAccordion>
 
 
         {/** Models */}
         <DynamicAccordion title={`3. Model Training & Prediction`} isOpenByDefault={true}>
-        <div className='mt-4'>
+        <div className='mt-6'>
             <img
                 src='/articles/NbaMvp/prediction.png'
                 alt='titlepic'
@@ -112,9 +113,9 @@ export default function TestBlogPost() {
           /></div>
           <p className="">{`The models appear overfitted, but they aren't predicting past winners and it is producing unique new players that we would want and expect to see, in Jayson Tatum and Anthony Davis.`}</p>
           <p className="">{`To reach these predictions, I chose three different models:`}</p>
-            <li className=''>Random Forest: These models essentially create multiple large decision trees and find the most likely path.  Usually better for a more complex and resource-intensive dataset but still useful here.</li>
-            <li className=''>Gradient Boosting: Slower than Random Forest if training large models, but typically provides better prediction accuracy.</li>
-            <li className=''>XGBoost: Incorporates L1 and L2 regularization to reduce overfitting, but can be sensitive to outliers.</li>
+            <li className='mb-2'><b>Random Forest:</b> These models essentially create multiple large decision trees and find the most likely path.  Usually better for a more complex and resource-intensive dataset but still useful here.</li>
+            <li className='mb-2'><b>Gradient Boosting:</b> Slower than Random Forest if training large models, but typically provides better prediction accuracy.</li>
+            <li className='mb-2'><b>XGBoost:</b> Incorporates L1 and L2 regularization to reduce overfitting, but can be sensitive to outliers.</li>
           <p className="">{`And there you have a simple model for predicting the NBA MVP!`}</p>
         </DynamicAccordion>
 
