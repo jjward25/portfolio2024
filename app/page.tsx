958c6e88fc1363a048c614392d0cbf319eb28aa2
jjward25 <45015765+jjward25@users.noreply.github.com>
2025-07-12 10:56:22 -0400

    d

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,14 @@ export default function Page() {
   return (
     <section className=''>
       
-      <div className="flex flex-row justify-between items-center">
+      <div className="flex flex-row justify-between items-center mt-4 md:mt-8 pb-4">
         <h1 className="font-medium text-2xl tracking-tighter text-black dark:text-white flex flex-row">Hey <p className='hover:rotate-45 hover:duration-500 transition-all ease-out mx-1'>👋</p> I'm Joe Ward</h1>
         <ContactButton />
       </div>
       
       <div className='mt-3 mb-3 h-0.5 w-full  bg-gradient-to-r from-amber-200 to-white'/>
 
-        <div className='flex my-8 md:my-8 flex-wrap place-content-center md:flex-nowrap '>
+        <div className='flex my-8 md:my-8 flex-wrap place-content-center md:flex-nowrap md:mt-16'>
           <div className="flex-none w-32 h-32 ml-10 mr-10 rounded-full overflow-hidden border-black dark:border-white border-2">
             <img
               src='/prof.jpg'
@@ -39,7 +39,7 @@ export default function Page() {
                     alt='prof'
                     className="w-auto h-6 object-cover ml-2"
                 /></div>
-                <p className="w-full h-10 pl-2 md:pl-6 inline-flex  items-center ">
+                <p className="w-full h-10 pl-2 md:pl-6 inline-flex items-center text-white">
                     Operational Excellence
                 </p>
             </div>
@@ -49,17 +49,17 @@ export default function Page() {
                   alt='prof'
                   className="w-auto h-5 object-cover ml-2"
                 /></div>
-                <p className="w-full h-10 pl-2 md:pl-6 inline-flex  items-center ">
+                <p className="w-full h-10 pl-2 md:pl-6 inline-flex  items-center text-white">
                   Bus. Intelligence & Growth
                 </p>
               </div>
             </ul>
 
             <ul className='w-full'>
-                <p className="cursor-pointer hover:shadow-md bg-cyan-950 hover:scale-105 w-full justify-center mb-3 inline-flex items-center rounded border  p-1 text-xs leading-4 border-neutral-700 no-underline bg-[linear-gradient(45deg,transparent_25%,rgba(255,255,255,.2)_50%,transparent_75%,transparent_100%)] bg-[length:250%_250%,100%_100%] bg-[position:-100%_0,0_0] bg-no-repeat shadow-xl transition-[background-position_0s_ease] hover:bg-[position:200%_0,0_0] hover:duration-[2000ms]">
+                <p className="cursor-pointer hover:shadow-md bg-cyan-950 hover:scale-105 w-full justify-center mb-3 inline-flex items-center rounded border  p-1 text-xs leading-4 border-neutral-700 no-underline bg-[linear-gradient(45deg,transparent_25%,rgba(255,255,255,.2)_50%,transparent_75%,transparent_100%)] bg-[length:250%_250%,100%_100%] bg-[position:-100%_0,0_0] bg-no-repeat shadow-xl transition-[background-position_0s_ease] hover:bg-[position:200%_0,0_0] hover:duration-[2000ms] text-white">
                   People & Project Manager
                 </p>
-                <p className="cursor-pointer hover:shadow-md bg-cyan-950 hover:scale-105 w-full justify-center inline-flex items-center rounded border  p-1 text-xs leading-4 border-neutral-700 no-underline bg-[linear-gradient(45deg,transparent_25%,rgba(255,255,255,.2)_50%,transparent_75%,transparent_100%)] bg-[length:250%_250%,100%_100%] bg-[position:-100%_0,0_0] bg-no-repeat shadow-xl transition-[background-position_0s_ease] hover:bg-[position:200%_0,0_0] hover:duration-[2000ms]">
+                <p className="cursor-pointer hover:shadow-md bg-cyan-950 hover:scale-105 w-full justify-center inline-flex items-center rounded border  p-1 text-xs leading-4 border-neutral-700 no-underline bg-[linear-gradient(45deg,transparent_25%,rgba(255,255,255,.2)_50%,transparent_75%,transparent_100%)] bg-[length:250%_250%,100%_100%] bg-[position:-100%_0,0_0] bg-no-repeat shadow-xl transition-[background-position_0s_ease] hover:bg-[position:200%_0,0_0] hover:duration-[2000ms] text-white">
                   Executive Partner
                 </p>
             </ul>
@@ -68,13 +68,13 @@ export default function Page() {
 
         <div className='mb-5 md:mb-0 h-0.5 md:h-0 w-full  bg-gradient-to-r from-white to-amber-200'> </div>
 
-      <div className='flex flex-nowrap h-auto'>
+      <div className='flex flex-nowrap h-auto my-16'>
         <div className='mt-1 md:mr-3 md:w-2 dark:md:w-1 md:h-vh bg-gradient-to-b from-amber-200 to-neutral-100'> </div>
         <div className='flex flex-wrap'>
-          <p className="pb-4 prose prose-invert w-full text-black dark:text-white">
+          <p className="pb-8 prose prose-invert w-full text-black dark:text-white">
             {`I'm a creative, detail-oriented, and technical business partner who's worked with teams from small non-profits, to the Fortune 500, to cutting-edge tech companies - helping them design and implement scalable operations, and get the most from what they have.`} 
           </p>
-          <p className="md:pb-4 prose prose-invert w-full text-black dark:text-white overflow-hidden h-auto">
+          <p className="md:pb-8 prose prose-invert w-full text-black dark:text-white overflow-hidden h-auto">
             {`Together, we can establish ground-level ops `}
             <em className='text-neutral-600'>{`(ex: process mapping; identifying KPIs and leading indicators (and building useful views); defining and enabling SOPs)`}</em>
             {`, or identify and tackle the more complex and tailored Operations improvements needed to hit ever-more challenging targets `}
